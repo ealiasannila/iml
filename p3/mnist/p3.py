7ac01249f6fc529449b606a83d63000f8733c55e
Elias Annila <elias.annila@gmail.com>
2015-11-11 09:05:54 +0200

    p3 done

--- p3/mnist/p3.py
+++ p3/mnist/p3.py
@@ -19,6 +19,13 @@ student_ID = '014328901'
 
 X, y = mnist.read_mnist_training_data(5000)
 
+trX = X[0:2500]
+trXf = [x.flatten() for x in trX]
+trY = y[0:2500]
+
+teX = X[2500:5000]
+teXf = [x.flatten() for x in teX]
+teY = y[2500:5000]
 
 
 def my_info():
@@ -27,14 +34,45 @@ def my_info():
     """
     return student_ID
 
+def doKNN(k):
+	dm = cdist(teXf, trXf,'euclidean')
+	cfm = np.zeros((10,10), dtype = int)
+	for	a in range(0,len(dm)):
+		knn = np.argpartition(dm[a], k)[:k]
+		preds = trY[knn]
+		counts = np.bincount(preds)
+		pred = -1
+		if len(counts)>=2:
+			top2 = np.argpartition(-counts, 1)	
+			if counts[top2[0]] == counts[top2[1]]:
+				d = 99999
+				for i in xrange(0,len(knn)):
+					val = dm[a][i]
+					if val < d:
+						d = dm[a][i]
+						pred = trY[knn[i]]
+			else:		
+				pred = top2[0]
+		else:
+			pred = 0
+		cfm[teY[a]][pred] += 1
+	print cfm
+	return cfm
+
 
 def KNN():
-    """
-    Implement the classifier using KNN and return the confusion matrix
-    :return: the confusion matrix regarding the result obtained using knn method
-    """
-    knn_conf_matrix = ''
-    return knn_conf_matrix
+	"""
+	Implement the classifier using KNN and return the confusion matrix
+	:return: the confusion matrix regarding the result obtained using knn method
+	"""
+	return doKNN(3)
+
+
+
+def verify(n, xs, ys):
+	for i in random.sample(range(0,len(xs)), n):
+		print ys[i]
+		mnist.visualize(xs[i])
 
 
 def simple_EC_classifier():
@@ -43,52 +81,23 @@ def simple_EC_classifier():
 	:return: the confusing matrix obtained regarding the result obtained using simple Euclidean distance method
 	"""
 
-	trX = X[0:2500]
-	trY = y[0:2500]
-
-	teX = X[2500:5000]
-	teXf = [x.flatten() for x in teX]
-	teY = y[2500:5000]
 	cfm = np.zeros((10,10), dtype = int)
 	digitn = [0]*10
-
-
 	trd = np.zeros((10,28*28), dtype = int)
-
-	def imgSum(digit, img):
+	for	i in xrange(0,len(trX)):
+		digit = trY[i]
+		digitn[digit] += 1
+		img = trX[i].flatten()
 		trd[digit] = np.sum([trd[digit], img], axis = 0)
-
-	def imgDiv(digit, n):
-		trd[digit] = np.divide(trd[digit], n)
-
-	def imgAvg():
-		for	i in xrange(0,len(trX)):
-			digit = trY[i]
-			digitn[digit] += 1
-			img = trX[i].flatten()
-			imgSum(digit, img)
-		for	d in xrange(0, len(trd)):
-			imgDiv(d, digitn[d])
-
-
-	def verify(n, xs, ys):
-		for i in random.sample(range(0,len(xs)), n):
-			print ys[i]
-			mnist.visualize(xs[i])
-
-	
-	imgAvg()
-
+	for	d in xrange(0, len(trd)):
+		trd[d] = np.divide(trd[d], digitn[d])
+		
 	dm = cdist(teXf, trd,'euclidean')
-
 	for	a in range(0,len(dm)):
 		pred = np.argmin(dm[a])
 		cfm[teY[a]][pred] += 1	
-
-
-
-	simple_EC_conf_martix = cfm
-	return simple_EC_conf_martix
+	print cfm
+	return cfm
 
 
 
@@ -99,7 +108,7 @@ def main():
     """
     results = my_info() + '\t\t'
     results += np.array_str(np.diagonal(simple_EC_classifier())) + '\t\t'
-    #results += np.array_str(np.diagonal(KNN()))
+    results += np.array_str(np.diagonal(KNN()))
     print results + '\n'
 
 if __name__ == '__main__':
